e782f4e7d666f0a07526e55729e951a263f2a825
kyleBSCS <kpvinuya@up.edu.ph>
2025-04-04 22:28:48 +0800

    ui: added background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-mono">
-      <div className="mx-auto mt-12 flex flex-col md:flex-row justify-center">
+    <div className="font-mono relative">
+      <div className=" mx-auto mt-12 flex flex-col md:flex-row justify-center">
         {/* Filter Box */}
         <FilterBox />
 
@@ -25,6 +25,16 @@ export default function Home() {
           <Card />
           <Card />
         </div>
+      </div>
+      {/* Background Image */}
+      <div className="fixed bottom-0 right-0 w-full h-full -z-10">
+        <Image
+          src="/pokeball.svg" // Change this to your actual image path
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-20"
+        />
       </div>
     </div>
   );
