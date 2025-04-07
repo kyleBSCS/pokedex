21edcb0bfb1dbe88f1babf62aaffd767b1c66937
kyleBSCS <kpvinuya@up.edu.ph>
2025-04-07 23:45:46 +0800

    feat: clicking on pokedex on topbar will scroll user to top

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -4,13 +4,22 @@ export default function TopBar() {
   // Define the polygon string directly for the clip-path style
   const clipPathStyle = `polygon(0% 0%, 100% 0%, 100% calc(100% - 2rem), calc(100% - 2rem) 100%, 0% 100%)`;
 
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div
       className="w-5/6 h-16 bg-white sticky top-0 border-b-2 border-r-2 border-black z-10 "
       style={{ clipPath: clipPathStyle }}
     >
       <div className="flex items-center justify-between h-full pl-12">
-        <h1 className="text-3xl font-bold font-mono text-gray-800">Pokédex</h1>
+        <button
+          onClick={handleScrollToTop}
+          className="text-3xl font-bold font-mono text-gray-800 focus:outline-none cursor-pointer"
+        >
+          Pokédex
+        </button>
         <div className="flex gap-2 pr-10">
           <div className="rounded-full w-6 h-6 bg-red-500 border-2"></div>
           <div className="rounded-full w-6 h-6 bg-yellow-500 border-2"></div>
